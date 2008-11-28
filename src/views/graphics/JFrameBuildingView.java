8651af674bd77f01ee9a8dac6d837ad2ce9cc9b3
Rémy Coutable <remy.coutable@gmail.com>
2008-11-28 13:53:56 +0100

    Ok

--- src/views/graphics/JFrameBuildingView.java
+++ src/views/graphics/JFrameBuildingView.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JFrame;
-
 import views.BuildingView;
 import views.ElevatorView;
 import models.Building;
