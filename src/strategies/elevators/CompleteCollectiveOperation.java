520925fd2d9525e7d44efb200b7302b2cddbb0e1
X-NeM <x_nem@MacBook-Pro-de-X-NeM.local>
2008-11-28 14:22:41 +0100

    from x-nem

---
+++ src/strategies/elevators/CompleteCollectiveOperation.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package strategies.elevators;
+
+/**
+ *
+ * @author x_nem
+ */
+public class CompleteCollectiveOperation {
+
+}
